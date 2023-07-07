e272af3dc6051fa679a90deef9626a280879ce1c
Kicka3 <kirill_ozornin@mail.ru>
2023-07-07 14:37:13 +0500

    js camp

---
+++ FreecodecampJS/67.js
@@ -0,0 +1,21 @@
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line\
+
+// Setup
+const testObj1 = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj1["an entree"];   // Change this line
+const drinkValue = testObj1["the drink"];    // Change this line
---
+++ FreecodecampJS/69.js
@@ -0,0 +1,22 @@
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+
+
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder"
+// Only change code below this line
