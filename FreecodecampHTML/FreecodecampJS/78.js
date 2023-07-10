c1898fd4918a6fa35eff4c4df462e9ba9b413303
Ozornin Kirill <kirill_ozornin@mail.ru>
2023-07-10 21:47:36 +0500

    htmlcss

---
+++ FreecodecampHTML/FreecodecampJS/78.js
@@ -0,0 +1,16 @@
+const myArray = [];
+
+// Only change code below this line.
+let i = 5;
+while(i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Setup
+const myArrayq = [];
+for (let i = 1; i <= 5; i++){
+  myArrayq.push(i);
+  console.log(myArrayq)
+}
+// Only change code below this line
