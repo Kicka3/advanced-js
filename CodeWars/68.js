97b76e775d663d78473886403c02c5182e1c64f9
Kicka3 <kirill_ozornin@mail.ru>
2023-07-16 22:29:46 +0500

    CWRS

---
+++ CodeWars/68.js
@@ -0,0 +1,12 @@
+function positiveSum(arr) {
+   console.log(arr)
+  
+    let counter = 0;
+   for (let i = 0; i < arr.length; i++){
+     if (arr[i] > 0){
+         counter += arr[i]
+         console.log(counter)
+     } 
+   }
+   return counter;
+ }
--- FreecodecampHTML/FreecodecampJS/161.js
+++ FreecodecampHTML/FreecodecampJS/161.js
@@ -14,15 +14,3 @@ function sumArray(array) {
   return 0;
 }
 
-function positiveSum(arr) {
-  console.log(arr)
- 
-   let counter = 0;
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] > 0){
-        counter += arr[i]
-        console.log(counter)
-    } 
-  }
-  return counter;
-}
