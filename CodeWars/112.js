78b134658099a94d604015cd0435da5248dcf100
Kicka3 <kirill_ozornin@mail.ru>
2023-07-22 20:12:35 +0500

    Codewars JS4

---
+++ CodeWars/112.js
@@ -0,0 +1,16 @@
+function arithmetic(a, b, operator){
+  switch (operator){
+      case "add":
+        return a + b
+        brake;
+      case "subtract":
+        return a - b
+        brake;
+      case "multiply":
+        return a * b
+        brake;
+      case "divide":
+        return a / b
+        brake;
+  }
+}
