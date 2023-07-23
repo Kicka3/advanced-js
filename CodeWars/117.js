5a29553ef1c93db20e8a59dad5ebae0cd0a8c619
Kicka3 <kirill_ozornin@mail.ru>
2023-07-23 14:43:37 +0500

    codewars

---
+++ CodeWars/117.js
@@ -0,0 +1,8 @@
+function addFive(num) {
+  
+  return num + 5
+}
+
+const addFive = num => num + 5;
+  
+  
---
+++ CodeWars/120.js
@@ -0,0 +1,12 @@
+function nameShuffler(str){
+  console.log(str)
+  
+  let a = str.split(' ').reverse().join(' ')
+  console.log(a)
+  
+  
+  return a;
+  
+     
+  
+}
