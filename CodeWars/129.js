0734234a74f922d77d7aefcbfe468de5a82f95a1
Ozornin Kirill <kirill_ozornin@mail.ru>
2023-07-24 23:04:02 +0500

    JSCodeWars1

--- CodeWars/129.js
+++ CodeWars/129.js
@@ -5,4 +5,11 @@ function zipWith(fn,a0,a1) {
     arr.push(fn(a0[i],a1[i]))
   }
   return arr
+}var locate = function(arr,value){
+  let arr2=[]
+  function flatten(arr){
+    return arr.map((a)=>Array.isArray(a)?flatten(a):arr2.push(a.toString()))
+  }
+  flatten(arr)
+  return arr2.some(v=>v===value.toString())
 }
