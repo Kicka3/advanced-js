4c9cb33fc1b4daea1159d6a8760d16142c72437c
Kicka3 <kirill_ozornin@mail.ru>
2023-07-26 22:26:06 +0500

    JSCodeWars40

--- CodeWars/148.js
+++ CodeWars/148.js
@@ -4,7 +4,9 @@ function grabDoll(dolls){
     if (dolls[i] == "Hello Kitty" || dolls[i] === "Barbie doll") {
       bag.push(dolls[i])
       continue;
-    } 
+    } else if (bag.lenght-1 == 3) {
+      break;
+    }
   } 
   
   return bag.slice(0,3);
@@ -13,3 +15,4 @@ function grabDoll(dolls){
 
 
 const grabDoll = dolls => dolls.filter(e => e == 'Hello Kitty' || e == 'Barbie doll').slice(0,3)
+
