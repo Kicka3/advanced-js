8c638826b7b5d4f22a083527c02cccea9a509f7b
Kicka3 <kirill_ozornin@mail.ru>
2023-07-27 20:52:09 +0500

    CodeWarsJS3

---
+++ CodeWars/151.js
@@ -0,0 +1,27 @@
+function colorOf(r,g,b){
+  let a = r.toString(16);
+  let e = g.toString(16);
+  let c = b.toString(16);
+  let n = '#';
+   return n + a + e + e + c + c;
+  
+  return '#' + fnc(r) + fnc(g) + fnc(b)
+  
+}
+
+function fnc(n) {
+  let result = n.toString(16);
+  return result.length == 1 ? '0' + result : result;
+}
+
+
+function colorOf(r,g,b){
+  
+  return '#' + fnc(r) + fnc(g) + fnc(b)
+  
+}
+
+function fnc(n) {
+  let result = n.toString(16);
+  return result.length == 1 ? '0' + result : result;
+}
