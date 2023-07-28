74e63543feebe996afc6337e134f4585973488ee
Kicka3 <kirill_ozornin@mail.ru>
2023-07-28 22:14:56 +0500

    CodeWars

--- CodeWars/154.js
+++ CodeWars/154.js
@@ -1,5 +1,3 @@
-const cutIt = arr => arr.map(element => element.slice(0,Math.min(...arr.map(element => element.length))))
-
 
 function ride(group,comet){
   let g= group.split``.reduce((a,b)=>a*(b.charCodeAt()-64),1)%47
