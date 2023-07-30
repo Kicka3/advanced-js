09bd890da2ef8ae5ace60aaa99e39068ec51c569
Kicka3 <kirill_ozornin@mail.ru>
2023-07-30 13:16:29 +0500

    codewars6

--- CodeWars/175.js
+++ CodeWars/175.js
@@ -1,5 +1,9 @@
-Number.prototype.add=function(x){ return this+x};
-Number.prototype.subtract=function(x){ return this-x;}
-Number.prototype.multiply=function(x){ return this*x;}
-Number.prototype.divide=function(x){ return this/x;}
-Number.prototype.square=function(){ return this*this;}
+function pattern(n){
+   var output="";
+     let arr = [];
+     for (let i=1,j=(n-1)*2-1;i<=n;i++,j-=2){
+       if (i===n) output+=' '.repeat(i-1)+i%10+' '.repeat(i-1)+'\n'
+       else output+=' '.repeat(i-1)+i%10+' '.repeat(j)+i%10+' '.repeat(i-1)+'\n'
+     }
+   return output+output.split`\n`.slice(0,-2).reverse().join`\n`;
+  }
