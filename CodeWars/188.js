5f09be85391f0cf29d1c837cbaa427cab35952e6
Kicka3 <kirill_ozornin@mail.ru>
2023-08-02 23:13:45 +0500

    codewars1

--- CodeWars/188.js
+++ CodeWars/188.js
@@ -1,10 +1,19 @@
-function combine(...arr) {
-    let newArr=[]
-    let max=Math.max(...arr.map(v=>v.length))
-    for(let i=0;i<max;i++){
-      for (let j=0;j<arr.length;j++){
-        arr[j][i]?newArr.push(arr[j][i]):1
-        }
-    }
-     return newArr
+function anagramDifference(str1,str2){
+  let count1 = Array(26).fill(0) 
+  let count2 = Array(26).fill(0) 
+  let i = 0
+  while (i < str1.length){
+      count1[str1[i].charCodeAt()-97] += 1
+      i += 1
+      }
+  i =0
+  while (i < str2.length){
+      count2[str2[i].charCodeAt()-97] += 1
+      i += 1
+      }
+ let result = 0
+  for (let i = 0; i<26;i++){
+      result += Math.abs(count1[i] - count2[i]) 
   }
+  return result 
+}
