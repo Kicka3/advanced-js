4f1812cd9e6114c8ea7c890e840ebe52af07d8c4
Kicka3 <kirill_ozornin@mail.ru>
2023-08-02 23:19:09 +0500

    codewars6

---
+++ CodeWars/194.js
@@ -0,0 +1,17 @@
+function isAlt(word) {
+ 
+  return word.split('').every((v,i)=>{
+  if (/[aeiou]/.test(word[0])){
+    if (i%2===0&&/[aeiou]/.test(v)){
+      return true
+      } else if (i%2!==0&&!/[aeiou]/.test(v)){ return true
+      } else {return false}
+    }
+  if (!/[aeiou]/.test(word[0])){
+    if (i%2==0&&!/[aeiou]/.test(v)){
+      return true
+      } else if (i%2!==0&&/[aeiou]/.test(v)){ return true
+      } else {return false}
+    }
+  })
+}
