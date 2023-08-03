9ee4796ce5c43585e723a9c71f747edf910f7443
Kicka3 <kirill_ozornin@mail.ru>
2023-08-03 21:28:09 +0500

    codewars5

--- CodeWars/201.js
+++ CodeWars/201.js
@@ -9,3 +9,7 @@ var alpha = {
         }.bind(this);
     }
 };
+
+function lostSheep(friday,saturday,total){
+  return total-(friday.reduce((acc,next)=>acc+next,0))-(saturday.reduce((acc,next)=>acc+next,0))
+}
