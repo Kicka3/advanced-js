a72c72b3e4d9c5709aad5e52f472a9b7d6e25d54
Kicka3 <kirill_ozornin@mail.ru>
2023-08-05 16:04:43 +0500

    Css15

---
+++ CodeWars/224.js
@@ -0,0 +1,15 @@
+function distinct(a) {
+  const newSet = [...new Set(a)]
+  console.log(newSet)
+}
+
+
+distinct([1,2,2,6,8,2]);
+distinct([1]);
+distinct([1,1,2,1,2,3,4,2]);
+
+// let chars = ['A', 'B', 'A', 'C', 'B'];
+// let dupChars = chars.filter((element, index) => {
+//     return chars.indexOf(element) !== index;
+// });
+// console.log(dupChars);
