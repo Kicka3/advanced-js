c2743b5f3918654e1459329aa01eb1f4898f9324
Kicka3 <kirill_ozornin@mail.ru>
2023-08-05 16:51:22 +0500

    sa

---
+++ CodeWars/226.js
@@ -0,0 +1,10 @@
+function lowercaseCount(str){
+  let lower = str.match(/[a-z]/g);
+if(lower === null) {
+    console.log(0)
+    return 0;
+} else {
+    console.log(lower.length)
+    return lower.length;
+}
+}
