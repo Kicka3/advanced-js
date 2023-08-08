9957f556ab707396da2ce05925dbe26fc393043e
Kicka3 <kirill_ozornin@mail.ru>
2023-08-08 16:59:55 +0500

    Cdwrs4

---
+++ CodeWars/320.js
@@ -0,0 +1,3 @@
+function playPass(s, n) {
+  return s.replace(/[A-Z]/g,x=>String.fromCharCode((x.charCodeAt(0)+n-65)%26+65)).replace(/\d/g,x=>9-x).split('').map((x,i)=>i%2?x.toLowerCase():x.toUpperCase()).reverse().join('');
+}
