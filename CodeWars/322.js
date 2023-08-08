02f3f0c2bf7e890144454519cac520936c87335d
Kicka3 <kirill_ozornin@mail.ru>
2023-08-08 17:03:20 +0500

    Cdwrs6

---
+++ CodeWars/322.js
@@ -0,0 +1,6 @@
+function colorProbability(color, texture){
+  if (texture==="smooth") return '0.33'
+  if (color==="red"&&texture==="bumpy") return '0.57'
+  if (color==="green"&&texture==="bumpy") return '0.14'
+  if (color==="yellow"&&texture==="bumpy") return '0.28'
+}
