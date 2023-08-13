b35d26847a6ac8fc12b15c418245a765ba12db7e
Kicka3 <kirill_ozornin@mail.ru>
2023-08-13 17:49:20 +0500

    DelNodemd

---
+++ Post/Post.tsx
@@ -0,0 +1,7 @@
+export const Post = () => {
+    return (
+        <div>
+            <h2>This is Post</h2>
+        </div>
+    )
+};
---
+++ User/User.tsx
@@ -0,0 +1,7 @@
+export const User = () => {
+    return (
+        <div>
+            <h2>This is User</h2>
+        </div>
+    )
+};
