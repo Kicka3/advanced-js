af3d14d1154e68fa2fbe5b2b3c561003e2b5a7b2
Kirill <kirill_ozornin@mail.ru>
2023-08-30 14:17:46 +0200

    CodeWars1

---
+++ CodeWars/448.js
@@ -0,0 +1,29 @@
+const isLeapYear = (year) => year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
+
+
+const str1 = "Кирилл"
+let splt2 = str.split('');
+console.log(splt);
+
+
+const str = "Кирилл"
+let splt = str.split('');
+console.log(splt);
+
+
+let rev = splt.reverse();
+console.log(rev);
+
+
+let join = rev.join('');
+console.log(join);
+
+
+const reverseWords = (str) => str.split
+
+function reverseWords(str) {
+
+   let result = str.split('').reverse().join('').split(' ').reverse().join(' ');
+   return result;
+
+}
