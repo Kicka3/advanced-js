0a2253ceba2eb400d34334507b3dc835f961f3a4
Kirill <kirill_ozornin@mail.ru>
2023-09-13 22:49:56 +0200

    7

--- CodeWars/478.js
+++ CodeWars/478.js
@@ -1,8 +1,11 @@
-function descendingOrder(n){
-  
-  return Number([...String(n)].sort((a,b) => b - a).join(''));
-  
-    
+
+
+function persistence(num) {
+  let result = 0;
+     str = String(num);
+ while (str.length > 1) {
+   str = String([...str].reduce((prev,curr) => prev * curr));
+   result ++;
+ }
+ return result
 }
-
-
