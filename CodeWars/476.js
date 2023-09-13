743b49f21f59b533077179adda876397ecef02d8
Kirill <kirill_ozornin@mail.ru>
2023-09-13 22:46:40 +0200

    2

---
+++ CodeWars/476.js
@@ -0,0 +1,8 @@
+function GrεεκL33t(str){
+   let dict = {a:'α',b:'β',d:'δ',e:'ε',i:'ι',k:'κ',n:'η',o:'θ',p:'ρ',r:'π',t:'τ',u:'μ',
+      if (dict[v.toLowerCase()]){
+         return v===v.toUpperCase()?dict[v.toLowerCase()]:dict[v]
+      }
+      return v.toLowerCase()
+   })
+}
