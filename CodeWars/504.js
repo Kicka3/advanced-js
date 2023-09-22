6aa332c894bb61e1109428094e2d68f0cc96cf8d
Kirill <kirill_ozornin@mail.ru>
2023-09-22 23:54:52 +0200

    CodeWars 2

---
+++ CodeWars/504.js
@@ -0,0 +1,10 @@
+function combine(...arr) {
+   let newArr=[]
+   let max=Math.max(...arr.map(v=>v.length))
+   for(let i=0;i<max;i++){
+      for (let j=0;j<arr.length;j++){
+         arr[j][i]?newArr.push(arr[j][i]):1
+      }
+   }
+   return newArr
+}
