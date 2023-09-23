1d7715f8377644825779ffcac19e360ca19894e8
Kirill <kirill_ozornin@mail.ru>
2023-09-23 20:38:47 +0200

    Solutions 4

--- CodeWars/110.js
+++ CodeWars/110.js
@@ -1,4 +1,5 @@
+function comp(array1, array2){
+   array1=array1.map(v=>v*v)
+   return array2?array1.reduce((a,b)=>a+b,0)===array2.reduce((a,b)=>a+b,0):false
+}
 
-function roundToNext5(n){
-  return Math.ceil(n/5)*5
- }
