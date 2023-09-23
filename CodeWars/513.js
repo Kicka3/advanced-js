5e804724d4b57c0999f4ffa48f697f06af8bbb49
Kirill <kirill_ozornin@mail.ru>
2023-09-23 20:39:12 +0200

    Solutions 5

---
+++ CodeWars/513.js
@@ -0,0 +1,12 @@
+function xor(a, b) {
+   if (a === b) return false;
+   return true;
+}
+
+const xor = (a, b) => a===b ? false : true;
+
+
+// function xor(a, b) {
+//   if (a === b) return false;
+//   return true;
+// }
