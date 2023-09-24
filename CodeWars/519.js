2b80e2e9006dca4973e0403d967c829c4ed0d6d4
Kirill <kirill_ozornin@mail.ru>
2023-09-24 21:54:45 +0200

    Solutions 3

---
+++ CodeWars/519.js
@@ -0,0 +1,7 @@
+const rgb = (r, g, b) => toHex(r) + toHex(g) + toHex(b);
+
+function toHex(numb) {
+   if (numb <= 0)   return '00';
+   if (numb > 255)  return 'FF';
+   return numb.toString(16).toUpperCase();
+}
