712a24abdd12b5113a5fbbc66043c5ef72f9542c
Kirill <kirill_ozornin@mail.ru>
2023-09-25 19:35:46 +0200

    Solutions 5

--- CodeWars/522.js
+++ CodeWars/522.js
@@ -1,3 +1,11 @@
-function areaCode(text) {
-   return text.match(/\((\d{3})\)/)[1];
-}
+
+const getMiddle = s => s.slice((s.length - 1) / 2, s.length / 2 + 1);
+
+function removeChar(str){
+   return str.slice(1,-1);
+
+};
+ 
+ 
+ 
+ 
