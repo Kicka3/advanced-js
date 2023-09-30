0d95b0a97a1c271bbb21e96e1234ff8b0222bdd7
Kirill <kirill_ozornin@mail.ru>
2023-09-30 20:51:01 +0200

    Solutions CodeWars 4

---
+++ CodeWars/557.js
@@ -0,0 +1,13 @@
+function points(games) {
+   let count = 0;
+   games.forEach(item => {
+      if (item[0] > item[2]) {
+         count +=3;
+      } else if(item[0] < item[2]) {
+         count += 0;
+      } else if (item[0] === item[2]) {
+         count += 1;
+      }
+   });
+   return count;
+}
