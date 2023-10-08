df27dcb1e2215294eb192903b4ac731fd04cb890
Kirill <kirill_ozornin@mail.ru>
2023-10-08 13:55:15 +0200

    Solutions CodeWars 5ku 2(v2)

--- CodeWars/626.js
+++ CodeWars/626.js
@@ -3,30 +3,29 @@ function fight(robot1, robot2, tactics) {
 
    let isDraw = false;
 
-   if(robot1.tactics.length == 0  && robot2.tactics.length == 0){
+   if (robot1.tactics.length == 0 && robot2.tactics.length == 0) {
       return 'The fight was a draw.';
-   }
-   else if(robot1.tactics.length == 0){
+   } else if (robot1.tactics.length == 0) {
       return robot2.name + ' has won the fight.';
-   }
-
-   else if(robot2.tactics.length == 0){
+   } else if (robot2.tactics.length == 0) {
       return robot1.name + ' has won the fight.';
    }
 
-   for(let i = 0; i < robot1.tactics.length; i++){
-      if((robot1.speed > robot2.speed) || (robot1.speed == robot2.speed) ){
+   for (let i = 0; i < robot1.tactics.length; i++) {
+      if ((robot1.speed > robot2.speed) || (robot1.speed == robot2.speed)) {
 
          robot2.health -= tactics[robot1.tactics[i]];
 
-         if(robot2.health <= 0){
+         if (robot2.health <= 0) {
             return robot1.name + ' has won the fight.';
          }
 
          robot1.health -= tactics[robot2.tactics[i]];
 
-         if(robot1.health <= 0){
+         if (robot1.health <= 0) {
             return robot2.name + ' has won the fight.';
          }
 
       }
+   }
+}
