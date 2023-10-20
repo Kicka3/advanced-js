34986a09072152d097f88df97b98aea1c74ecf02
Kirill <kirill_ozornin@mail.ru>
2023-10-20 23:04:03 +0200

    Solutions CodeWars es sol 5

--- CodeWars/685.js
+++ CodeWars/685.js
@@ -1,11 +1,22 @@
-//1. Создайте полную (глубокую) копию объекта user
-let deepCopyUser = structuredClone(user);
-const deepCopyUser2 = JSON.parse(JSON.stringify(user))
-console.log("1=============================================")
+function comp(array1, array2){
+   let result = false;
 
-
-//2. Создайте полную (глубокую) массива students
-let deepCopyStudents = students.map(el => ({...el}));
-console.log(deepCopyStudents)
-console.log(11111111111111111)
-console.log("2=============================================")
+   if ( array1 && array2 ) {
+      if (array1.length === 0 && array2.length === 0){
+         result = true;
+      } else {
+         const squareRoots = array2.map(num => Math.sqrt(num));
+         array1.sort((a, b) => a-b);
+         squareRoots.sort((a, b) => a-b);
+         for (let i=0; i<squareRoots.length; i++) {
+            if (array1.indexOf(squareRoots[i])
+               !== array1.indexOf(array1[i])) {
+               result = false;
+               break;
+            }
+            result = true;
+         }
+      }
+   }
+   return result;
+}
