49be059e1afae1f72b0a7826e451b49284ed41e5
Kirill <kirill_ozornin@mail.ru>
2023-10-21 23:44:48 +0200

    Solutions CodeWars es sol 3

--- CodeWars/649.js
+++ CodeWars/649.js
@@ -1,12 +1,22 @@
+function comp(array1, array2){
+   let result = false;
 
-let friends = ["Ryan", "Kieran", "Mark", "Diana", "Kirya",];
-
-function friend(friends){
-   let result = friends.filter(words => words.length === 4);
-   console.log(result);
+   if ( array1 && array2 ) {
+      if (array1.length === 0 && array2.length === 0){
+         result = true;
+      } else {
+         const squareRoots = array2.map(num => Math.sqrt(num));
+         array1.sort((a, b) => a-b);
+         squareRoots.sort((a, b) => a-b);
+         for (let i=0; i<squareRoots.length; i++) {
+            if (array1.indexOf(squareRoots[i])
+               !== array1.indexOf(array1[i])) {
+               result = false;
+               break;
+            }
+            result = true;
+         }
+      }
+   }
+   return result;
 }
-
-
-friend(friends);
-
-const friend = friends => friends.filter(words => words.length == 4);
