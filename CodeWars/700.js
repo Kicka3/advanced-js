9c30f45232975787c703a4a36885b74674bdfe48
Kirill <kirill_ozornin@mail.ru>
2023-10-26 23:15:02 +0200

    CodeWars 10

---
+++ CodeWars/700.js
@@ -0,0 +1,9 @@
+function getGrade(...scores) {
+   let average = scores.reduce((a, b) => a + b) / scores.length
+
+   if (average >= 90) return 'A'
+   else if (average >= 80) return 'B'
+   else if (average >= 70) return 'C'
+   else if (average >= 60) return 'D'
+   else return 'F'
+}
