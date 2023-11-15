dbd50d2d01e26139345a66937a9f5191c0a77e4c
Kirill <kirill_ozornin@mail.ru>
2023-11-15 22:31:51 +0200

    Second task

---
+++ CodeWars/750.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+   if (number % 2 == 0) {
+      console.log("Even");
+      return "Even";
+   } else if (number % 2  !== 0) {
+      console.log("Odd");
+      return "Odd"
+   }
+}
+
+evenOrOdd(8);
+
+const evenOrOdd = number => number % 2 == 0 ? "Even" : "Odd";
