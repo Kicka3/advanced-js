09af62fa507fa4d6e606bdc780a30b1d1f9a04fe
Kirill <kirill_ozornin@mail.ru>
2023-11-28 23:57:46 +0200

    task three

---
+++ CodeWars/762.js
@@ -0,0 +1,29 @@
+
+let year1 = '1705'
+let year2 = '1900'
+let year3 = '1601'
+let year4 = '2000'
+
+
+
+function century(year) {
+   if(year % 2 == 0) {
+      let odd = year.split('').slice(0,2).reduce((acc, curr) => acc + curr)
+      console.log(odd)
+      return odd;
+   } else {
+      let even = year.split('').slice(0,2).reduce((summ, curr) => summ + curr)
+      let resB = Number(even);
+      let result = resB + 1;
+      console.log(result)
+      return result;
+   }
+
+
+   return;
+}
+
+century(year1)
+century(year2)
+century(year3)
+century(year4)
