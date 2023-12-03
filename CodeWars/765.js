d78563a20cdbdc962098134a520972a376f10b49
Kirill <kirill_ozornin@mail.ru>
2023-12-03 23:21:15 +0200

    cd wrs2

---
+++ CodeWars/765.js
@@ -0,0 +1,10 @@
+
+function tribonacci(signature,n){
+   let arr=[...signature];
+   for (let i=0;i<n;i++){
+      arr.push(arr.slice(i,i+3).reduce((acc,next)=>acc+next,0))
+   }
+
+   return arr.slice(0,-3)
+}
+
