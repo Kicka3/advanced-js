61205f2976de70f2319eaed1dc0cc12456f22108
Kirill <kirill_ozornin@mail.ru>
2023-12-07 23:18:34 +0200

    getMiddle

--- CodeWars/771.js
+++ CodeWars/771.js
@@ -1,20 +1,10 @@
-const findSpaceship = (map = "") => {
-   map = map.split("\n").reverse()
 
-   for (let y = 0; y < map.length; y++) {
-      for (let x = 0function GrεεκL33t(str){
-         let dict = {a:'α',b:'β',d:'δ',e:'ε',i:'ι',k:'κ',n:'η',o:'θ',p:'ρ',r:'π',t:'τ',u:'μ',
-            v:'υ',w:'ω',x:'χ',y:'γ'}
-         return str.replace(/./g,v=>{
-            if (dict[v.toLowerCase()]){
-               return v===v.toUpperCase()?dict[v.toLowerCase()]:dict[v]
-            }
-            return v.toLowerCase()
-         })
-      }; x < map[y].length; x++) {
-         if (map[y][x] === "X") return [x, y]
-      }
-   }
+const getMiddle = s => s.slice((s.length - 1) / 2, s.length / 2 + 1);
 
-   return "Spaceship lost forever."
-}
+function removeChar(str){
+   return str.slice(1,-1);
+
+};
+
+
+
