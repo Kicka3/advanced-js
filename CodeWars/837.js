da0aa6a9df1bfc33b516664e8c6ffc36336ef121
Kirill <kirill_ozornin@mail.ru>
2024-01-13 00:00:45 +0500

    refactoring battle

---
+++ CodeWars/837.js
@@ -0,0 +1,10 @@
+
+
+function battle(x, y) {
+   a = x.split('').map(el => el.charCodeAt(0) - 64).reduce((a, b) => a + b, 0)
+
+   b = y.split('').map(el => el.charCodeAt(0) - 64).reduce((a, b) => a + b, 0)
+   return a < b ? y : a > b ? x : 'Tie!'
+}
+
+console.log(battle("ONE", "TWO"))
