b71828f539f3d9abf9188d7a1c2efb39db7556b5
Kirill <kirill_ozornin@mail.ru>
2024-01-15 00:06:18 +0500

    refac

--- CodeWars/821.js
+++ CodeWars/821.js
@@ -1,12 +1,12 @@
 add(1); // 1
 // We should be able to store the returned values and reuse them.
 
-var addTwo = add(2);
-addTwo; // 2
-addTwo + 5; // 7
-addTwo(3); // 5
-addTwo(3)(5); // 10
-// We can assume any number being passed in will be valid whole number.
+// var addTwo = add(2);
+// addTwo; // 2
+// addTwo + 5; // 7
+// addTwo(3); // 5
+// addTwo(3)(5); // 10
+//
 
 
 var add = function(n) {
