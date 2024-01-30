837212c3fe66eb28b15712723bbfe4ffa5fe083d
Kirill <kirill_ozornin@mail.ru>
2024-01-30 23:05:39 +0500

    merryChristmas

---
+++ CodeWars/858.js
@@ -0,0 +1,12 @@
+function merryChristmas(s1,s2){
+   let check = 'Merry Christmas!'.split``.sort().join``
+   for (let i=0;i<s2.length;i++){
+      let a=new RegExp(`${s2[i]}`)
+      if (s1.includes(s2[i])){
+         s1=s1.replace(a,'')
+      } else{
+         s1+=s2[i]
+      }
+   }
+   return  check===s1.split``.sort().join``
+}
