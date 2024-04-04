8d6539710ac5c178c79aa102dbf100993487972f
Kirill <kirill_ozornin@mail.ru>
2024-04-04 23:07:24 +0500

    solve

---
+++ CodeWars/876.js
@@ -0,0 +1,11 @@
+function solve(arr) {
+
+   const map = { Begin: 'Begin', Left: 'Right', Right: 'Left' }
+      ,   parts = arr.map(step => step.split(' on '))
+      ,   steps = [];
+
+   for (let i = 0; i < arr.length; i++)
+      steps.unshift(map[parts[(i + 1) % arr.length][0]] + ' on ' + parts[i][1]);
+
+   return steps;
+}
