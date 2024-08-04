683576112b5f015b0654cf006f3ce6f734291fdb
Kirill <kirill_ozornin@mail.ru>
2024-08-04 10:17:33 +0500

    feat: authList2

--- CodeWars/934.js
+++ CodeWars/934.js
@@ -1,5 +1,6 @@
 function authList(arr) {
-   return arr.every( (x) => {
-      return /^(?=.*\d+)(?=.*[a-z])[a-z\d]{6,10}$/.test(x)
+   return arr.every( (y) => {
+
+      return /^(?=.*\d)(?=.*[a-z])[a-z\d]{6,10}$/.test(y)
    })
 }
