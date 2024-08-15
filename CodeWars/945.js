1b199408627d346fe6a2be92ccb60883fea0ad29
Kirill <kirill_ozornin@mail.ru>
2024-08-15 11:46:03 +0500

    feat: scoreToTally

---
+++ CodeWars/945.js
@@ -0,0 +1,24 @@
+let scoreToTally = function(score){
+   let arr =[];
+   while (score>=5){
+      arr.push('e <br>')
+      score-=5
+   }
+   while (score>=4){
+      arr.push('d')
+      score-=4
+   }
+   while (score>=3){
+      arr.push('c')
+      score-=3
+   }
+   while (score>=2){
+      arr.push('b')
+      score-=2
+   }
+   while (score>=1){
+      arr.push('a')
+      score-=1
+   }
+   return arr.join(``)
+}
