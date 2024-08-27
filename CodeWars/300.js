413bfdab7d594487434ee6de044357d94b809b36
Kirill <kirill_ozornin@mail.ru>
2024-08-27 23:36:40 +0500

    feat: evenOrOdd

--- CodeWars/300.js
+++ CodeWars/300.js
@@ -1,3 +1,13 @@
-function encrypt(text, rule) {
-  return text.split('').map(v=>String.fromCharCode((v.charCodeAt()+rule)%256)).join(``)
-};
+function evenOrOdd(number) {
+  if (number % 2 == 0) {
+    console.log("Even");
+    return "Even";
+  } else if (number % 2  !== 0) {
+    console.log("Odd");
+    return "Odd"
+  }
+}
+
+evenOrOdd(8);
+
+const evenOrOdd = number => number % 2 == 0 ? "Even" : "Odd";
