696fdda3deb8693c06bf21b66f86d16c71f591ed
Kirill <kirill_ozornin@mail.ru>
2024-09-29 21:58:46 +0500

    feat: Lamp

--- CodeWars/979.js
+++ CodeWars/979.js
@@ -1,6 +1,6 @@
-function game(nobi){
-   let tie=nobi.filter(v=>v==='()').length
-   let f=nobi.filter(v=>v==='8<').length*2+tie
-   let s=nobi.filter(v=>v==='[]').length*2+tie
-   return [s,f]
+class Lamp{
+   constructor(color,state){
+      this.color=color
+      this.state=state
+   }
 }
