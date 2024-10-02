9f36023bd7e5ac4f1e69ef8cab3813581506ff1c
Kirill <kirill_ozornin@mail.ru>
2024-10-02 21:18:30 +0500

    feat: toaster

--- CodeWars/981.js
+++ CodeWars/981.js
@@ -1,7 +1,4 @@
-function sample(coin) {
-   let head = 0;
-   for (let i = 0; i < 8000; i++) {
-      if (coin()) { head++; }
-   }
-   return head / 8000;
-}
+let toaster=(b)=>{
+   b=b.toLowerCase()
+   return b=="white bread"?"1 min":b=="frozen white bread"?"1.5 mins":
+      b=="brown bread"?"2 mins":b=="frozen brown bread"?"3 mins":'Please do not put that in the toaster!'}
