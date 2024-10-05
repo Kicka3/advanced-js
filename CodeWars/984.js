1eb8cb9bc12f3e079e6e3c952ed4f5607cde134a
Kirill <kirill_ozornin@mail.ru>
2024-10-05 10:30:25 +0500

    feat: splitExp

--- CodeWars/984.js
+++ CodeWars/984.js
@@ -1,9 +1,17 @@
-function factors(n) {
-   let arrSQ=[];
-   let arrCB=[];
-   for (let i=2;i<n;i++){
-      if (n%(i*i)===0) arrSQ.push(i)
-      if (n%(i*i*i)===0) arrCB.push(i)
+function splitExp(n) {
+   let str=n.split('.')
+   let arr1 = [];
+   let arr2 = [];
+   for (let i=0;i<str[0].length;i++){
+      arr1.push(str[0].slice(i,i+1)+'0'.repeat(str[0].length-1-i))
    }
-   return [arrSQ,arrCB]
+   arr1=arr1.filter(v=>v*1!==0)
+   if (str[1]){
+      for (let i=0;i<str[1].length;i++){
+         arr2.push('.'+'0'.repeat(i)+str[1].slice(i,i+1))
+      }
+      arr2=arr2.filter(v=>v*1!==0)
+      return arr1.concat(arr2)
+   }
+   return arr1
 }
