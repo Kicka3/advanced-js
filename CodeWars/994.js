bc4230278aaed65d950c257d98501b9a62aae782
Kirill <kirill_ozornin@mail.ru>
2024-10-15 23:50:50 +0500

    feat: European Arabic Numerals Encoder Decoder

---
+++ CodeWars/994.js
@@ -0,0 +1,10 @@
+ArabicNumerals = {
+   // accepts a number and returns a string in Arabic Numerals
+   encode: function (x) {
+      return x+''
+   },
+   // accepts a string in Arabic numerals and returns a number
+   decode: function (x) {
+      return x*1
+   }
+};
