c118d29119ed2d1cce42c3aca4e10a1b1e55da6e
Kirill <kirill_ozornin@mail.ru>
2024-10-22 23:28:32 +0500

    feat: buildTableText

---
+++ CodeWars/1000.js
@@ -0,0 +1,5 @@
+function buildTableText(row, column, character) {
+   let arr = str.split(`---------\n`);
+   arr[row+1]= arr[row+1].split(' ').map((v,i)=>i===column?v='|'+character:v).join(` `).replace(`${character} `,`${character}`)
+   return arr.join(`---------\n`);
+}
