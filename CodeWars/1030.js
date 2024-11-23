c9432b1825ec94b8f76ed38a28455341b5581e46
Kirill <kirill_ozornin@mail.ru>
2024-11-23 18:56:28 +0500

    feat: xMarksTheSpot

---
+++ CodeWars/1030.js
@@ -0,0 +1,12 @@
+const xMarksTheSpot = (input) => {
+   let count=0;
+   let x=0;
+   let y=0;
+   input.map((v,i)=>{
+      if (v.includes('x')){
+         v.map((x,i)=>x==='x'?y=i:v)
+         count++;
+         x=i;}
+      else return v})
+   return count===0?[]:count>1?[]:[x,y]
+}
